dfb3e069ef1788d011bd54d46481f24cf6ae9cc5
AbhirupBhowmick <abhirupbhowmick111777@gmail.com>
2025-06-30 20:02:24 +0530

    Fix: Set host to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -28,5 +28,9 @@ def index():
         price = model.predict(X_latest)[0]
     return render_template('index.html', price=price, symbol=stock_symbol)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
